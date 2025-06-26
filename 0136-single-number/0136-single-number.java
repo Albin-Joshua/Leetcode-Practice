46b69542be4af15b1ad2868f6025ebab37c203c0
Albin-Joshua <113009336+Albin-Joshua@users.noreply.github.com>
2025-06-26 11:57:14 +0530

    Time: 1 ms (99.92%), Space: 46.6 MB (17.54%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,16 +1,22 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int size = nums.length;
-        Map<Integer, Integer> map = new HashMap<>();
-        for(int n: nums){
-            map.put(n, map.getOrDefault(n,0)+1);
+        int result = 0;
+        for(int i=0; i<nums.length; i++){
+            result = result^nums[i];
         }
-        for(int i=0; i<size; i++){
-            if(map.get(nums[i])==1){
-                return nums[i];
-            }
-        }
-        return -1;
+        return result;
+
+        // int size = nums.length;
+        // Map<Integer, Integer> map = new HashMap<>();
+        // for(int n: nums){
+        //     map.put(n, map.getOrDefault(n,0)+1);
+        // }
+        // for(int i=0; i<size; i++){
+        //     if(map.get(nums[i])==1){
+        //         return nums[i];
+        //     }
+        // }
+        // return -1;
 
         // int size = nums.length;
         // Arrays.sort(nums);
@@ -24,10 +30,6 @@ class Solution {
         // }
         // return -1;
 
-        // int result = 0;
-        // for(int i=0; i<nums.length; i++){
-        //     result = result^nums[i];
-        // }
-        // return result;
+
     }
 }
