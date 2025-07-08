ac450dcef93ca2f141347cd6d8b3f818a585e13f
Albin-Joshua <113009336+Albin-Joshua@users.noreply.github.com>
2025-07-08 10:56:28 +0530

    Time: 16 ms (94.18%), Space: 55 MB (65.08%) - LeetHub

--- 0917-boats-to-save-people/0917-boats-to-save-people.java
+++ 0917-boats-to-save-people/0917-boats-to-save-people.java
@@ -1,17 +1,47 @@
 class Solution {
     public int numRescueBoats(int[] people, int limit) {
         Arrays.sort(people);
-        int l = 0;
-        int r = people.length - 1;
-        int result = 0;
+        int count = 0;
+        int l=0;
+        int r=people.length - 1;
         while(l<=r){
-            int remain = limit - people[r];
-            r--;
-            result++;
-            if(l<=r && remain >= people[l]){
+            if(people[l]+people[r]<=limit){
+                count++;
                 l++;
+                r--;
+            }
+            else{
+                count++;
+                r--;
             }
         }
-        return result;
+        return count;
+
+        // int m = Arrays.stream(people).max().getAsInt();
+        // int[] count = new int[m+1];
+        // for(int p:people){
+        //     count[p]++;
+        // }
+        // int idx = 0, i=1;
+        // while(idx<people.length){
+        //     while(count[i]==0){
+        //         i++;
+        //     }
+        //     people[idx] = i;
+        //     idx++;
+        //     count[i]--;
+        // }
+        // int l = 0;
+        // int r = people.length - 1;
+        // int result = 0;
+        // while(l<=r){
+        //     int remain = limit - people[r];
+        //     r--;
+        //     result++;
+        //     if(l<=r && remain >= people[l]){
+        //         l++;
+        //     }
+        // }
+        // return result;
     }
 }
