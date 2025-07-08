cc3b5ddaab10a1b14be3748fb076b66162bd832a
Albin-Joshua <113009336+Albin-Joshua@users.noreply.github.com>
2025-07-08 14:30:50 +0530

    Time: 1 ms (99.86%), Space: 48 MB (88.17%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -11,19 +11,52 @@
  */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        Set<ListNode> set = new HashSet<>();
         ListNode n1 = headA;
+        ListNode n2 = headB;
+        int count1 = 0;
+        int count2 = 0;
         while(n1 != null){
-            set.add(n1);
+            count1++;
             n1 = n1.next;
         }
-        ListNode n2 = headB;
         while(n2 != null){
-            if(set.contains(n2)){
-                return n2;
-            }
+            count2++;
             n2 = n2.next;
         }
+        ListNode a1 = headA;
+        ListNode a2 = headB;
+        while(count1>count2){
+            a1 = a1.next;
+            count1--;
+        }
+        while(count2>count1){
+            a2 = a2.next;
+            count2--;
+        }
+        while(a1 != null || a2 != null){
+            if(a1 == a2){
+                return a1;
+            }
+            else{
+                a1 = a1.next;
+                a2 = a2.next;
+            }
+        }
         return null;
+
+        // Set<ListNode> set = new HashSet<>();
+        // ListNode n1 = headA;
+        // while(n1 != null){
+        //     set.add(n1);
+        //     n1 = n1.next;
+        // }
+        // ListNode n2 = headB;
+        // while(n2 != null){
+        //     if(set.contains(n2)){
+        //         return n2;
+        //     }
+        //     n2 = n2.next;
+        // }
+        // return null;
     }
 }
