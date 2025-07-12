10e763ac7c85c00887f059b75cc29fd8ff865b52
Albin-Joshua <113009336+Albin-Joshua@users.noreply.github.com>
2025-07-12 09:25:41 +0530

    Time: 0 ms (100%), Space: 41 MB (9.99%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,4 +1,19 @@
 class Solution {
+//below method if recursive method, which has TC of O(2^(m+n)), leads to TLE
+
+    // public static int func(int i,int j,int m,int n){
+    //     if(i>=m || j>=n) return 0;
+    //     if(i+1==m && j+1==n) return 1;
+    //     int right = func(i,j+1,m,n);
+    //     int down = func(i+1,j,m,n);
+    //     return right+down;
+    // }
+
+    // public int uniquePaths(int m, int n) {
+    //     int ans = func(0,0,m,n);
+    //     return ans;
+    // }
+
     public int uniquePaths(int m, int n) {
         int[][] arr = new int[m][n];
         for(int i=0; i<m; i++){
