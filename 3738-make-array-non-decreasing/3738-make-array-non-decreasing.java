a5beb80534764dacad2596d674c928ce51ea5ef6
Albin-Joshua <113009336+Albin-Joshua@users.noreply.github.com>
2025-07-31 17:54:48 +0530

    Time: 3 ms (68.42%), Space: 72.6 MB (80.04%) - LeetHub

---
+++ 3738-make-array-non-decreasing/3738-make-array-non-decreasing.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maximumPossibleSize(int[] nums) {
+        int size=0;
+        int prev = -1;
+        for(int num:nums){
+            if(num>=prev){
+                prev=num;
+                size++;
+            }
+        }
+        return size;
+    }
+}
