8e352abb9ccdf8859f64346af7cbe9e1e90a12e9
Albin-Joshua <113009336+Albin-Joshua@users.noreply.github.com>
2025-07-31 11:56:15 +0530

    Time: 2 ms (55.46%), Space: 45 MB (43.42%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,12 +1,18 @@
+/*At each step, multiply current number with both curMax and curMin — because a negative number can flip min to max or vice versa.
+
+Update curMax as the best positive product ending here, and curMin as the worst (most negative).
+
+Keep res to track the overall max product seen so far.*/
+
 public class Solution {
     public int maxProduct(int[] nums) {
         int res = nums[0];
-        int curMin = 1, curMax = 1;
-
-        for (int num : nums) {
-            int tmp = curMax * num;
-            curMax = Math.max(Math.max(tmp, num * curMin), num);
-            curMin = Math.min(Math.min(tmp, num * curMin), num);
+        int curMin = 1;
+        int curMax = 1;
+        for(int num:nums){
+            int temp = curMax*num;
+            curMax = Math.max(Math.max(temp, curMin*num), num);
+            curMin = Math.min(Math.min(temp, curMin*num), num);
             res = Math.max(res, curMax);
         }
         return res;
