49d54b2145a644a707351ed5219075c10792c47a
Albin-Joshua <113009336+Albin-Joshua@users.noreply.github.com>
2025-08-24 19:09:37 +0530

    Time: 0 ms (100%), Space: 42 MB (92.18%) - LeetHub

--- 0171-excel-sheet-column-number/0171-excel-sheet-column-number.java
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.java
@@ -1,11 +1,9 @@
 class Solution {
     public int titleToNumber(String columnTitle) {
-        int value = 0;
-        for (int i = 0; i < columnTitle.length(); i++) {
-            if (i > 0) {
-                value = value * 26;
-            }
-            value += (columnTitle.charAt(i) - 'A' + 1);
+        int value=0;
+        for(int i=0; i<columnTitle.length(); i++){
+            value = value*26;
+            value += columnTitle.charAt(i) - 'A' + 1;
         }
         return value;
     }
